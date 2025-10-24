eda83eef8003a58aafb4b2fc4feb729c9522543b
Oleksandra Stepanova <128746781+OleksandraStepanova@users.noreply.github.com>
2025-10-24 17:53:54 +0200

    Add constants

---
+++ src/js/products-api.js
@@ -0,0 +1,12 @@
+export const API_BASE_URL = 'https://dummyjson.com';
+
+
+export const API_ENDPOINTS = {
+  PRODUCTS: '/products',
+  PRODUCT_BY_ID: '/products/',
+  CATEGORIES: '/products/category-list',
+  PRODUCTS_BY_CATEGORY: '/products/category/',
+  SEARCH: '/products/search',
+};
+
+export const ITEMS_PER_PAGE = 12;
